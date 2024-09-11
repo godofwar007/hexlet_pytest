017032765b4641604984454184d0e720883aba73
Peter_007 <hihb3ya@gmail.com>
2024-09-11 15:02:05 +0300

    added new test by "open with"

--- tests/test_example.py
+++ tests/test_example.py
@@ -3,7 +3,13 @@ from hextel_pytest.example import reverse
 
 def test_reverse():
     assert reverse('Hexlet') == 'telxeH'
-
-
-def test_reverse_for_empty_string():
     assert reverse('') == ''
+
+
+def test_reverse_with_file():
+    with open('fixtures/forreverse.txt', 'r', encoding='utf-8') as f:
+        result = f.read().strip()
+
+    test1 = reverse(result).strip()
+    exepted_result = result[::-1]
+    assert test1 == exepted_result
